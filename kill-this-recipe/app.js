cd95887f158075b94160bfc13d55919747335689
Brett Gurzick <59893116+bgurzick@users.noreply.github.com>
2024-06-17 15:50:51 -0700

    commit

--- kill-this-recipe/app.js
+++ kill-this-recipe/app.js
@@ -7,6 +7,13 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 
+// some silly placeholder data
+let posts = [
+  { id: 1, title: 'To Die For Super Bowl Dip', content: 'Every year in, I think January? Or is it February? Anyway, my family and I...' },
+  { id: 2, title: 'Black Bean Soup', content: "If you don't like black bean soup, you should just stop reading now!" },
+];
+
+
 
 // start server
 app.listen(port, () => {
